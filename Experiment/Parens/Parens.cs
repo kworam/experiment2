799fd56fe6e36a5e1ea66af3a30f9827959bb8e6
KevinWoram <Kevin.Woram@RelayHealth.com>
2018-07-03 12:07:21 -0600

    Added Parens and its unit tests

---
+++ Experiment/Parens/Parens.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Experiment.Parens
+{
+	public class Parens
+	{
+		public static List<string> GetParens(int n)
+		{
+			if (n < 0)
+			{
+				return null;
+			}
+
+			if (n == 0)
+			{
+				return new List<string>() { "" };
+			}
+
+			List<string> result = new List<string>();
+			foreach (string s in GetParens(n - 1))
+			{
+				result.Add("(" + s + ")");
+				if (s.Length > 0)
+				{
+					result.Add("()" + s);
+					if (MaxDepth(s) > 1)
+					{
+						result.Add(s + "()");
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static int MaxDepth(string s)
+		{
+			int maxDepth = 0;
+			int currentDepth = 0;
+			foreach (char c in s)
+			{
+				if (c == '(')
+				{
+					currentDepth++;
+				}
+				else
+				{
+					 currentDepth--;
+				}
+				maxDepth = Math.Max(maxDepth, currentDepth);
+			}
+
+			return maxDepth;
+		}
+
+		public static List<string> GetParensBook(int n)
+		{
+			if (n < 0)
+			{
+				return null;				
+			}
+
+			List<string> result = new List<string>();
+			char[] parens = new char[n*2];
+			int index = 0;
+			GetParensBookInternal(index, parens, n, n, result);
+			return result;
+		}
+
+		private static void GetParensBookInternal(
+			int index, char[] parens, int leftRemaining, int rightRemaining, List<string> result)
+		{
+			if (leftRemaining < 0 || rightRemaining < 0)
+			{
+				// error
+				return;
+			}
+
+			if (leftRemaining == 0 && rightRemaining == 0)
+			{
+				result.Add(new string(parens));
+				return;
+			}
+
+			parens[index] = '(';
+			GetParensBookInternal(index + 1, parens, leftRemaining - 1, rightRemaining, result);
+
+			parens[index] = ')';
+			GetParensBookInternal(index + 1, parens, leftRemaining, rightRemaining - 1, result);
+		}
+
+
+		public static List<string> GetParensBookKevin(int n)
+		{
+			if (n < 0)
+			{
+				return null;
+			}
+
+			List<string> result = new List<string>();
+			char[] parens = new char[n * 2];
+			int index = 0;
+			int leftRemaining = n;
+			int rightAvailable = 0;
+			GetParensBookInternalKevin(index, parens, leftRemaining, rightAvailable, result);
+			return result;
+		}
+
+		private static void GetParensBookInternalKevin(
+			int index, char[] parens, int leftRemaining, int rightAvailable, List<string> result)
+		{
+			if (leftRemaining == 0 && rightAvailable == 0)
+			{
+				result.Add(new string(parens));
+				return;
+			}
+
+			if (leftRemaining > 0)
+			{
+				parens[index] = '(';
+				GetParensBookInternalKevin(index + 1, parens, leftRemaining - 1, rightAvailable + 1, result);
+			}
+
+			if (rightAvailable > 0)
+			{
+				parens[index] = ')';
+				GetParensBookInternalKevin(index + 1, parens, leftRemaining, rightAvailable - 1, result);
+			}
+		}
+	}
+}
---
+++ ExperimentUnitTest/Parens/ParensUnitTest.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using Experiment.Parens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperimentUnitTest.Parens
+{
+	[TestClass]
+	public class ParensUnitTest
+	{
+		[TestCategory("Parens"), TestMethod]
+		public void Negative()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(-1);
+			Assert.IsNull(parens);
+			List<string> parensBook = Experiment.Parens.Parens.GetParensBook(-1);
+			Assert.IsNull(parensBook);
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(-1);
+			Assert.IsNull(parensBookKevin);
+		}
+
+		[TestCategory("Parens"), TestMethod]
+		public void Zero()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(0);
+			Assert.AreEqual(parens.Count, 1);
+			Assert.AreEqual(parens[0], string.Empty);
+
+			List<string> parensBook = Experiment.Parens.Parens.GetParensBook(0);
+			Assert.AreEqual(parensBook.Count, 1);
+			Assert.AreEqual(parensBook[0], string.Empty);
+
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(0);
+			Assert.AreEqual(parensBookKevin.Count, 1);
+			Assert.AreEqual(parensBookKevin[0], string.Empty);
+		}
+
+		[TestCategory("Parens"), TestMethod]
+		public void One()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(1);
+			Assert.AreEqual(parens.Count, 1);
+
+			//List<string> parensBook = Experiment.Parens.Parens.GetParensBook(1);
+			//Assert.AreEqual(parensBook.Count, 1);
+
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(1);
+			Assert.AreEqual(parensBookKevin.Count, 1);
+		}
+
+		[TestCategory("Parens"), TestMethod]
+		public void Two()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(2);
+			Assert.AreEqual(parens.Count, 2);
+
+			//List<string> parensBook = Experiment.Parens.Parens.GetParensBook(2);
+			//Assert.AreEqual(parensBook.Count, 2);
+
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(2);
+			Assert.AreEqual(parensBookKevin.Count, 2);
+		}
+
+		[TestCategory("Parens"), TestMethod]
+		public void Three()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(3);
+			Assert.AreEqual(parens.Count, 5);
+
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(3);
+			Assert.AreEqual(parensBookKevin.Count, 5);
+		}
+
+		[TestCategory("Parens"), TestMethod]
+		public void Four()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(4);
+			//Assert.AreEqual(parens.Count, 14);
+
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(4);
+			//Assert.AreEqual(parensBookKevin.Count, 14);
+		}
+
+		[TestCategory("Parens"), TestMethod]
+		public void Five()
+		{
+			List<string> parens = Experiment.Parens.Parens.GetParens(5);
+			//Assert.AreEqual(parens.Count, 5);
+
+			List<string> parensBookKevin = Experiment.Parens.Parens.GetParensBookKevin(5);
+			//Assert.AreEqual(parensBookKevin.Count, 42);
+		}
+	}
+}
