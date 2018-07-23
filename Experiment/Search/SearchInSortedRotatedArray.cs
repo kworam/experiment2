257720d73661d26357ad294e51f3f1c69f0b4fa0
KevinWoram <Kevin.Woram@RelayHealth.com>
2018-07-23 13:29:07 -0600

    Added Search Class and Unit Tests

---
+++ Experiment/Search/SearchInSortedRotatedArray.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Schema;
+
+namespace Experiment.Search
+{
+	public class SearchInSortedRotatedArray
+	{
+		public static int Search(int[] ra, int val)
+		{
+			// assumption: ra is an array sorted in ascending order that has been rotated by an unknown amount
+			if (ra == null || ra.Length == 0) return -1;
+
+			int minIndex = FindMinIndex(ra);
+			return InternalSearch(ra, val, 0, ra.Length - 1, minIndex);
+		}
+
+		private static int InternalSearch(int[] ra, int val, int start, int end, int offset)
+		{
+			while (start <= end)
+			{
+				int mid = (start + end) / 2;
+				int adj = (mid + offset) % ra.Length;
+				if (ra[adj] < val)
+				{
+					start = mid + 1;
+				}
+				else if (ra[adj] > val)
+				{
+					end = mid - 1;
+				}
+				else
+				{
+					return adj;
+				}
+			}
+
+			return -1;
+		}
+
+
+		private static int FindMinIndex(int[] ra)
+		{
+			return FindMinIndexInternal(ra, 0, ra.Length - 1);
+		}
+
+		private static int FindMinIndexInternal(int[] ra, int start, int end)
+		{
+			if (end == start || end == start + 1) return end;
+
+			int mid = (start + end) / 2;
+			if (ra[start] > ra[mid]) return FindMinIndexInternal(ra, start, mid);
+			if (ra[mid] > ra[end]) return FindMinIndexInternal(ra, mid, end);
+			return start;
+		}
+	}
+}
---
+++ ExperimentUnitTest/Search/SearchUnitTest.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using Experiment.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperimentUnitTest.Search
+{
+	[TestClass]
+	public class SearchUnitTest
+	{
+		[TestCategory("Search"), TestMethod]
+		public void NullArray()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(null, 1), -1);
+		}
+
+		[TestCategory("Search"), TestMethod]
+		public void EmptyArray()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(
+				GenerateRotatedSortedArray(length: 0, rotation: 0), val: 1), -1);
+		}
+
+		[TestCategory("Search"), TestMethod]
+		public void SingletonArrayNotFound()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 1, rotation: 1), val: 99), -1);
+		}
+
+
+		[TestCategory("Search"), TestMethod]
+		public void SingletonArrayFound()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 1, rotation: 2), val: 0), 0);
+		}
+
+		[TestCategory("Search"), TestMethod]
+		public void TwoItemArrayNotFound()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 2, rotation: 1), val: 5), -1);
+		}
+
+		[TestCategory("Search"), TestMethod]
+		public void TwoItemArrayFound()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 2, rotation: 1), val: 0), 1);
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 2, rotation: 1), val: 1), 0);
+		}
+
+		[TestCategory("Search"), TestMethod]
+		public void LargeArrayNotFound()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 100, rotation: 13), val: 107), -1);
+		}
+
+		[TestCategory("Search"), TestMethod]
+		public void LargeArrayFound()
+		{
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 100, rotation: 13), val: 99), (99 + 13) % 100);
+			Assert.AreEqual(SearchInSortedRotatedArray.Search(GenerateRotatedSortedArray(length: 100, rotation: 13), val: 55), (55 + 13) % 100);
+		}
+
+		private int[] GenerateRotatedSortedArray(int length, int rotation)
+		{
+			int[] sorted = Enumerable.Range(0, length).ToArray();
+			RotateArray(sorted, rotation);
+			return sorted;
+		}
+
+		private static void ReverseArray(int[] a, int start, int end)
+		{
+			if (a == null)
+			{
+				return;
+			}
+
+			while (start < end)
+			{
+				int tmp = a[start];
+				a[start] = a[end];
+				a[end] = tmp;
+				start++;
+				end--;
+			}
+		}
+
+		private static void RotateArray(int[] a, int rotation)
+		{
+			if (a == null || a.Length < 2 || (rotation % a.Length) == 0)
+			{
+				return;
+			}
+
+			ReverseArray(a, 0, a.Length - 1);
+			ReverseArray(a, 0, rotation - 1);
+			ReverseArray(a, rotation, a.Length - 1);
+		}
+	}
+}
