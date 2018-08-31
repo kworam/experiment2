5c6357c4b941cfec37f3c2a4a0b6c6127f31df2c
KevinWoram <Kevin.Woram@RelayHealth.com>
2018-08-31 10:53:28 -0600

    add primality problem/unit tests

---
+++ Experiment/HackerRank/Primality.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Experiment.HackerRank
+{
+	public class Primality
+	{
+		public static string primality(int n)
+		{
+			// Sieve of erastothenes
+			// if # is 1, 2
+			if (n == 1)
+			{
+				return "Not prime";
+			}
+
+			if (n == 2)
+			{
+				return "Prime";
+			}
+
+			// for all numbers n > 2
+			//   create a bit vector of size 0..sqrt(n)
+			//   for each number x from 2..sqrt(n), set all multiples of x to false
+			//   if x is prime, sqrt(x) is not an integer
+			//   any numbers > sqrt(x) will not divide the prime evenly
+			//int numInts = (int) Math.Ceiling((double)n/32);
+			BitArray bitVector = new BitArray(n+1);
+			int stop = (int)Math.Ceiling(Math.Sqrt(n));
+			for (int i = 2; i <= stop; i++)
+			{
+				if (n%i == 0) return "Not prime";
+				//EliminateMultiplesOf(i, bitVector);
+			}
+
+			//return bitVector[n] ? "Not prime" : "Prime";
+			return "Prime";
+		}
+		static void EliminateMultiplesOf(int i, BitArray bitVector)
+		{
+			int multiple = 2;
+			while (i * multiple < bitVector.Length)
+			{
+				bitVector[i * multiple] = true;
+				multiple++;
+			}
+		}
+	}
+}
---
+++ ExperimentUnitTest/HackerRank/PrimalityUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Experiment.HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperimentUnitTest.HackerRank
+{
+	[TestClass]
+	public class PrimalityUnitTest
+	{
+		[TestCategory("Primality"), TestMethod]
+		public void TestCase7()
+		{
+			using (StreamReader sr = new StreamReader(@"HackerRank\Primality_TestCase7.txt"))
+			{
+				int p = Convert.ToInt32(sr.ReadLine());
+
+				for (int tItr = 0; tItr < p; tItr++)
+				{
+					int n = Convert.ToInt32(sr.ReadLine());
+					Console.WriteLine(Primality.primality(n));
+				}
+			}
+		}
+	}
+}
