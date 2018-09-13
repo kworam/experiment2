94125748c5c34e83cacbb8883ed9a2ec0eeaceef
KevinWoram <Kevin.Woram@RelayHealth.com>
2018-09-13 11:38:11 -0600

    Add MaxFriendCircle problem and solution

---
+++ Experiment/HackerRank/MaxFriendCircle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExperimentUnitTest.HackerRank
+{
+	internal class MaxFriendCircle
+	{
+		public static int[] maxCircle(int[][] queries)
+		{
+			int maxFriendCircle = int.MinValue;
+			int[] maxes = new int[queries.Length];
+			Dictionary<int, UnionFindNode> people = new Dictionary<int, UnionFindNode>();
+			int j = 0;
+			for (int i = 0; i < queries.Length; i++)
+			{
+				UnionFindNode p1 = AddPerson(queries[i][0], people);
+				UnionFindNode p2 = AddPerson(queries[i][1], people);
+				UnionFindNode p1Root = p1.GetRoot();
+				UnionFindNode p2Root = p2.GetRoot();
+				if (p1Root.id != p2Root.id)
+				{
+					UnionFindNode newRoot = Union(p1Root, p2Root);
+					maxFriendCircle = Math.Max(maxFriendCircle, newRoot.size);
+				}
+
+				maxes[i] = maxFriendCircle;
+			}
+
+			return maxes;
+		}
+
+		private class UnionFindNode
+		{
+			public int id;
+			public int size;
+			public UnionFindNode parent;
+
+			public UnionFindNode(int id)
+			{
+				this.id = id;
+				this.size = 1;
+				this.parent = this;
+			}
+
+			public UnionFindNode GetRoot()
+			{
+				UnionFindNode root = this;
+				while (root.parent != root)
+				{
+					root = root.parent;
+				}
+				return root;
+			}
+
+			public override string ToString()
+			{
+				return string.Format("{0}_{1}", this.id,this.parent.id);
+			}
+		}
+
+		private static UnionFindNode AddPerson(int id, Dictionary<int, UnionFindNode> people)
+		{
+			if (!people.ContainsKey(id))
+			{
+				people[id] = new UnionFindNode(id);
+			}
+			return people[id];
+		}
+
+		private static UnionFindNode Union(UnionFindNode n1, UnionFindNode n2)
+		{
+			UnionFindNode smaller = n1.size < n2.size ? n1 : n2;
+			UnionFindNode larger = n1 == smaller ? n2 : n1;
+			smaller.parent = larger;
+			larger.size += smaller.size;
+			return larger;
+		}
+	}
+}
---
+++ ExperimentUnitTest/HackerRank/MaxFriendCircleUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Experiment.HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperimentUnitTest.HackerRank
+{
+	[TestClass]
+	public class MaxFriendCircleUnitTest
+	{
+		[TestCategory("MaxFriendCircle"), TestMethod]
+		public void TestCase0()
+		{
+			using (StreamReader sr = new StreamReader(@"HackerRank\MaxFriendCircle_TestCase0.txt"))
+			{
+				DoTest(sr);
+			}
+		}
+
+		[TestCategory("MaxFriendCircle"), TestMethod]
+		//[Timeout(1000)]
+		public void TestCase2()
+		{
+			using (StreamReader sr = new StreamReader(@"HackerRank\MaxFriendCircle_TestCase2.txt"))
+			{
+				DoTest(sr);
+			}
+		}
+
+		private static void DoTest(StreamReader sr)
+		{
+			int q = Convert.ToInt32(sr.ReadLine());
+
+			int[][] queries = new int[q][];
+
+			for (int i = 0; i < q; i++)
+			{
+				queries[i] = Array.ConvertAll(sr.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+			}
+
+			int[] ans = MaxFriendCircle.maxCircle(queries);
+			Console.WriteLine(string.Join("\n", ans));
+		}
+	}
+}
